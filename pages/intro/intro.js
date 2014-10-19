23e1f3acce4cf4e4f1ddbe8675aa451bf8320956
Parashuram <code@r.nparashuram.com>
2014-10-18 18:18:01 -0700

    From refactor in master branch

--- pages/intro/intro.js
+++
@@ -1,7 +0,0 @@
-(function() {
-	console.log($('.link-activity-list, .link-intro'));
-	$('.link-activity-list, .link-intro').on('click', function() {
-		$('.logo').toggle();
-		$('.activities').toggle();
-	});
-}());
